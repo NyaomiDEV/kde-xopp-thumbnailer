226ef14c664ed28e48715c2e10319de89c075049
Naomi Calabretta <me@arytonex.pw>
2022-01-11 22:27:36 +0100

    generate thumbs using xournalpp directly
    i want them big

--- xoppthumbnailer.cpp
+++ xoppthumbnailer.cpp
@@ -17,7 +17,7 @@ XoppThumbnailer::XoppThumbnailer(){}
 
 XoppThumbnailer::~XoppThumbnailer(){}
 
-bool XoppThumbnailer::create(const QString& path, int /*width*/, int /*heigth*/, QImage& img){
+bool XoppThumbnailer::create(const QString& path, int width, int height, QImage& img){
     QString cacheDirectory((QStandardPaths::writableLocation(QStandardPaths::GenericCacheLocation) + "/thumbnails/xoppthumbnailer/"));
     QString md5Hash = QString(QCryptographicHash::hash((path.toUtf8()), QCryptographicHash::Md5).toHex());
     QString thumbnailPath(cacheDirectory + md5Hash + ".png");
@@ -29,12 +29,12 @@ bool XoppThumbnailer::create(const QString& path, int /*width*/, int /*heigth*/,
     if (!QFile::exists(thumbnailPath)){
         QProcess *process = new QProcess();
         process->setProcessChannelMode(QProcess::ForwardedChannels);
-        process->start("xournalpp-thumbnailer", {path, thumbnailPath});
+        process->start("xournalpp", {path, "-i", thumbnailPath, "--export-range", "1"});
         process->waitForFinished();
     }
 
     QFile thumbnailFile(thumbnailPath);
-    img = QImage(thumbnailPath);
+    img = QImage(thumbnailPath).scaled(width, height, Qt::KeepAspectRatio, Qt::SmoothTransformation);
     return !img.isNull();
 }
 
